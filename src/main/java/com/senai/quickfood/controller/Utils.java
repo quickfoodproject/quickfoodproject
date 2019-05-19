6a39e3f74cc1fc8a5773c66ac77abcad3abf554c
gregoryalves <gregoryalves@hotmail.com>
2019-05-19 17:12:04 -0300

    Tratamento de erro na criptografia de senha

--- src/main/java/com/senai/quickfood/controller/Utils.java
+++ src/main/java/com/senai/quickfood/controller/Utils.java
@@ -6,17 +6,29 @@ import java.security.NoSuchAlgorithmException;
 
 public class Utils {
 
-    public static String criptografarSHA256(String pConteudo) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        StringBuilder sb = new StringBuilder();
+    public static String criptografarSHA256(String pConteudo) {
+        String senhaHex = "";
+        
+        try {
+            StringBuilder sb = new StringBuilder();
 
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        byte[] encodedhash = md.digest(pConteudo.getBytes("UTF-8"));
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
 
-        for (byte b : encodedhash) {
-            sb.append(String.format("%02X", 0xFF & b));
+            try {
+                byte[] encodedhash = md.digest(pConteudo.getBytes("UTF-8"));
+
+                for (byte b : encodedhash) {
+                    sb.append(String.format("%02X", 0xFF & b));
+                }
+
+            } catch (UnsupportedEncodingException e) {
+                System.err.println("Não foi possível criptografar o conteúdo: UnsupportedEncodingException: " + e.toString());
+            }
+
+            senhaHex = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Não foi possível criptografar o conteúdo: NoSuchAlgorithmException: " + e.toString());
         }
-
-        String senhaHex = sb.toString();
 
         return senhaHex;
     }
